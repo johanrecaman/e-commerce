83474bafc5206a07cf03e10b6c746374bad71e94
Johan Recaman <johanstrr@gmail.com>
2023-10-26 23:57:41 -0300

    login logic started

--- lib/php/login.php
+++ lib/php/login.php
@@ -1,20 +1,28 @@
 <?php
+session_start();
 include("connect.php");
 
 $email = $_POST['email'];
 $password = md5($_POST['password']);
 
-$sql = "SELECT tipo FROM usuarios WHERE email = $email AND senha = $password";
+$sql = "SELECT tipo FROM usuarios WHERE email = '$email' AND senha_hash = '$password'";
 $result = mysqli_query($conn, $sql);
 
 if($row = mysqli_fetch_row($result)){
-    $userType = $row[0];
+    $_SESSION['user_type'] = $row[0];
+    if($_SESSION['user_type'] == 'adm'){
+        echo json_encode("adm");
+    }
+    elseif($_SESSION['user_type'] == 'user'){
+        echo json_encode("user");
+    }
+    else{
+        echo json_encode("error");
+    }
 }
 else{
-    $userType = 0;
+    echo json_encode("error");
 }
-
 mysqli_close($conn);
-
-echo $userType;
+exit();
 ?>
