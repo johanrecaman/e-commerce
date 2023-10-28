a10d95f599bd3854b1519712066d696325fcf1d2
Johan Recaman <johanstrr@gmail.com>
2023-10-28 19:17:16 -0300

    test

--- lib/php/login.php
+++ lib/php/login.php
@@ -2,27 +2,51 @@
 session_start();
 include("connect.php");
 
-$email = $_POST['email'];
-$password = md5($_POST['password']);
+function login($conn){
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
 
-$sql = "SELECT tipo FROM usuarios WHERE email = '$email' AND senha_hash = '$password'";
-$result = mysqli_query($conn, $sql);
+    $sql = "SELECT tipo FROM usuarios WHERE email = '$email' AND senha_hash = '$password'";
+    $result = mysqli_query($conn, $sql);
 
-if($row = mysqli_fetch_row($result)){
-    $_SESSION['user_type'] = $row[0];
-    if($_SESSION['user_type'] == 'adm'){
-        echo json_encode("adm");
-    }
-    elseif($_SESSION['user_type'] == 'user'){
-        echo json_encode("user");
+    if($row = mysqli_fetch_row($result)){
+        $_SESSION['user_type'] = $row[0];
+        if($_SESSION['user_type'] == 'adm'){
+            return json_encode("adm");
+        }
+        elseif($_SESSION['user_type'] == 'user'){
+            return json_encode("user");
+        }
     }
     else{
-        echo json_encode("error");
+        return json_encode("error");
     }
 }
-else{
-    echo json_encode("error");
+
+function logout(){
+    session_destroy();
+
 }
-mysqli_close($conn);
-exit();
+
+function isLoggedIn(){
+    return json_encode($_SESSION['user_type']);
+}
+
+
+$action = $_GET['action'];
+
+if($action == 'login'){
+    echo login($conn);
+    mysqli_close($conn);
+    exit();
+}
+else if($action == 'isLoggedIn'){
+    echo isLoggedIn();
+    exit();
+}
+else if($action == 'logout'){
+    logout();
+    exit();
+}
+
 ?>
