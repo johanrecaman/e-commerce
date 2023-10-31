07b8bc89e25eaa3eb33f90f7b219037e8049b856
Johan Recaman <johanstrr@gmail.com>
2023-10-30 22:02:45 -0300

    cart products added dinamically

--- lib/php/cart.php
+++ lib/php/cart.php
@@ -1,12 +1,48 @@
 <?php
 include("connect.php");
+function addToCart($conn){
+    $userId = $_POST['userId'];
+    $productId = $_POST['productId'];
+    $quantity = $_POST['quantity'];
 
-$userId = $_POST['userId'];
-$productId = $_POST['productId'];
-$quantity = $_POST['quantity'];
+    $sql = "INSERT INTO carrinho_de_compras (usuario_id, produto_id, quantidade) VALUES ('$userId', '$productId', '$quantity')";
+    mysqli_query($conn, $sql);
 
-$sql = "INSERT INTO carrinho_de_compras (usuario_id, produto_id, quantidade) VALUES ('$userId', '$productId', '$quantity')";
-mysqli_query($conn, $sql);
+}
 
-echo "success";
+function getCartProducts($conn){
+    $userId = $_POST["userId"];
+
+    $sqlCartSelect = "SELECT produto_id FROM carrinho_de_compras WHERE usuario_id = '$userId'";
+    $resultCart = mysqli_query($conn, $sqlCartSelect);
+
+    $products = array();
+    while($rowCart = mysqli_fetch_assoc($resultCart)){
+        $productId = $rowCart['produto_id'];
+        $sqlProductSelect = "SELECT * FROM produtos WHERE id = '$productId'";
+        $resultProduct = mysqli_query($conn, $sqlProductSelect);
+        while($rowProduct = mysqli_fetch_assoc($resultProduct)){
+            $products[] = array(
+                'id' => $rowProduct['id'],
+                'name'=> $rowProduct['nome'],
+                'price' => $rowProduct['preco'],
+                'img_url' => $rowProduct['url_imagem']
+            );
+        }  
+    }
+    return $products;
+}
+
+$action = $_GET["action"];
+
+if($action == "addToCar"){
+    addToCart($conn);
+    mysqli_close($conn);
+    exit();
+}
+elseif($action == "getCartProducts"){
+    echo json_encode(getCartProducts($conn));
+    mysqli_close($conn);
+    exit();
+}
 ?>
